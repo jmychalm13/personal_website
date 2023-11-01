8ff31100e6281be94c356c32af859fd1cecd0360
“Jessica <jmychalm13@gmail.com>
2023-10-31 19:12:09 -0500

    added toggle navbar for projects and pokedex to projects

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const open_btn = document.querySelector(".open-btn");
 const close_btn = document.querySelector(".close-btn");
 const nav = document.querySelectorAll(".nav");
+const dropdown = document.getElementById("dropdown");
+const projects = document.getElementById("projects");
+
+dropdown.addEventListener("click", () => {
+  projects.classList.toggle("hidden");
+});
 
 open_btn.addEventListener("click", () => {
   nav.forEach((nav_el) => nav_el.classList.add("visible"));
